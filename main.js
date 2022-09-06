62aa9ce86933a5925c9977f16c4a8c471179f2e0
shubham4511 <89202701+shubham4511@users.noreply.github.com>
2022-09-06 19:17:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+noseX=0;
+noseY=0;
+diff=0;
+rightwristX=0;
+leftwristX=0;
+
+function setup() {
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotposes);
+}
+function draw(){
+    background("grey");
+    fill('yellow');
+    stroke('yellow');
+    shubham(noseX,noseY,diff);
+    
+}
+function modelLoaded(){
+    console.log("Posenet is InitiaLize");
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        rightwristX=results[0].pose.rightWrist.x;
+        leftwristX=results[0].pose.leftWrist.x;
+        diff=floor(leftwristX-rightwristX);
+    }
+}
